a0ebea7f0879362ceb758dd29db96e8aca02ff68
Claudio Roldán Ramos <claudio.roldan@atos.net>
2022-02-04 12:18:29 +0000

    feat: :rocket: Controlador para eliminar proyectos.

--- app/Controllers/Http/ProyectosController.ts
+++ app/Controllers/Http/ProyectosController.ts
@@ -3,27 +3,40 @@
 import Auth from "App/Models/Auth";
 import Proyecto from "App/Models/Proyecto";
 
-
 export default class ProyectosController {
-
-  public async index({auth}){
-    let user:Auth = auth.use('api').user;
-    return await user.related('proyectos').query()
+  public async index({ auth }) {
+    let user: Auth = auth.use("api").user;
+    return await user.related("proyectos").query();
   }
 
-  public async create( {auth, request} ){
-    const user:Auth = auth.use('api').user;
+  public async create({ auth, request }) {
+    const user: Auth = auth.use("api").user;
     const { nombre } = request.all();
     const proyecto = new Proyecto();
     proyecto.fill({
-      nombre
+      nombre,
     });
-    await user.related('proyectos').save(proyecto);
+    await user.related("proyectos").save(proyecto);
     return proyecto;
   }
 
-  public async destroy ( { auth, request, params}){
-    const user:Auth = auth.use('api').user;
+  public async destroy({ auth, response, params }) {
+    const user: Auth = auth.use("api").user;
+    const { id } = params;
+    const proyecto = await Proyecto.find(id);
+    if (proyecto != null) {
+      if (proyecto.authId == user.id) {
+        await proyecto.delete();
+      }else{
+        return response.status(403).json({
+          mensaje: "No puede eliminar un proyecto que no le pertenece"
+        })
+      }
+    }else{
+      return response.status(404).json({
+        mensaje: "No existe ningún proyecto con este id"
+      })
+    }
+    return proyecto;
   }
-
 }
