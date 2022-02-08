54a2ed1327d908621cc7913dbfe9c269f928c17a
Claudio Roldán Ramos <claudio.roldan@atos.net>
2022-02-08 09:29:22 +0000

    fix: Condicional operator !==

--- app/Services/AutorizacionService.ts
+++ app/Services/AutorizacionService.ts
@@ -2,7 +2,7 @@ import AccesoProhibidoException from "App/Exceptions/AccesoProhibidoException";
 
 export default class AutorizacionService{
   public static verificarPermiso(recurso, user){
-    if (recurso.authId != user.id) {
+    if (recurso.authId !== user.id) {
       throw new AccesoProhibidoException("No puede acceder al recurso: ".concat(recurso), 403);
     }
   }
